f0ada273dc9e1527d1c00ff7fcb37c3cfaa6b669
KURRUHEM <45422379+KURRUHEM@users.noreply.github.com>
2025-07-05 15:22:23 +0530

    Delete src/config/database.js

--- src/config/database.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const connectDB = async () => {
-  await mongoose.connect("mongodb+srv://nodetutorial:NJf209e0NHulyKyE@nodetutorial.wxyafkx.mongodb.net/nodepractise")
-}
-
-module.exports = {
-  connectDB
-}
